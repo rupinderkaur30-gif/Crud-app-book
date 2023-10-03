7253f73077f6ef24e67e2f37789f05302140b3c8
rupinderkaur <rupi063094@gmail.com>
2023-10-03 17:53:10 -0400

    added update and delete method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,45 @@
 class BooksController < ApplicationController
-    before_action :set_book, only: [:show, :edit, :update]
+    before_action :set_book, only: [:show, :edit, :update, :destroy]
     def index
         @books = Book.all
     end
 
+    def new
+        @book = Book.new
+    end
+
+    def create
+       book = Book.create(book_params)
+       redirect_to book
+    end
     def show
         @book = Book.find(params[:id])
     end
 
     def edit
-        byebug
+        
+    end
+
+    def update
+       @book.update(book_params)
+       redirect_to(book_params)
+    end
+
+
+    def destroy
+    
+      @book.destroy
+      redirect_to books_path
     end
 
     private 
 
     def set_book
         @book = Book.find(params[:id])
+
+    end
+
+    def book_params
+        params.require(:book).permit(:title,  :genre, :author, :years_published)
     end
 end
